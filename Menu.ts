38166ad4753b81f0d0892704d2d3df8831967f52
mwendell <mwendellsmce@gmail.com>
2026-02-04 15:41:47 -0300

    :sparkles: Criação da classe conta e testes no menu

--- Menu.ts
+++ Menu.ts
@@ -1,24 +1,48 @@
 import leia from "readline-sync"
 import { Colors } from './src/util/Colors'
+import { Conta } from "./src/model/Conta";
 
 let opcao: number
-//menu do while
+
+//instaciar objetos da classe conta
+
+const c1 = new Conta(1, 1234, "Sofia", 1, 100000.00);
+
+//teste metodo sacar
+console.log('Sacar 100.00: ', c1.sacar(100));
+console.log('Sacar 200000.00', c1.sacar(200000));
+console.log('Sacar 0:', c1.sacar(0));
+
+//teste do metodo depositar
+
+console.log('Depositar -10: ')
+c1.depositar(-10)
+
+console.log('Depositar 500: ')
+c1.depositar(500)
+
+c1.visualizar
+
 do {
-    console.log(Colors.bg.green, Colors.fg.yellow,
-        '*****************************************          \n')
-    console.log('          BANCO DO BRAZIL COM Z            ')
-    console.log('\n*****************************************\n')
-    console.log('1 - criar conta')
-    console.log('2 - Listar todas as contas')
-    console.log('3 - Buscar conta por Numero')
-    console.log('4 - Atualizar dados da conta')
-    console.log('5 - Apagar conta')
-    console.log('6 - Sacar')
-    console.log('7 - Depositar')
-    console.log('8 - Transferir')
-    console.log('9 - Sair')
-    console.log('*****************************************      ')
-    console.log('                                               ',
+    console.log(Colors.bg.green, Colors.fg.yellow, 
+                    '*****************************************************')
+        console.log('                                                     ')
+        console.log('                BANCO DO BRAZIL COM Z                ')
+        console.log('                                                     ')
+        console.log('*****************************************************')
+        console.log('                                                     ')
+        console.log('            1 - Criar Conta                          ')
+        console.log('            2 - Listar todas as Contas               ')
+        console.log('            3 - Buscar Conta por Numero              ')
+        console.log('            4 - Atualizar Dados da Conta             ')
+        console.log('            5 - Apagar Conta                         ')
+        console.log('            6 - Sacar                                ')
+        console.log('            7 - Depositar                            ')
+        console.log('            8 - Transferir valores entre Contas      ')
+        console.log('            9 - Sair                                 ')
+        console.log('                                                     ')
+        console.log('*****************************************************')
+        console.log('                                                     ', 
         Colors.reset)
 
     opcao = leia.questionInt(Colors.fg.blue + 'Escolha a opcao: ' + Colors.reset)
--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,3 +1,110 @@
+import { Colors } from "../util/Colors";
+
 export class Conta{
     
+    // atributos da classe
+
+    private _numero: number;
+    private _agencia: number;
+    private _titular: string;
+    private _tipo: number;
+    private _saldo: number;
+
+    // metodo construtor
+
+	constructor(numero: number, agencia: number, titular: string, tipo: number, saldo: number) {
+		this._numero = numero;
+		this._agencia = agencia;
+		this._titular = titular;
+		this._tipo = tipo;
+		this._saldo = saldo;
+	}
+
+    // Métodos Get e Set
+	public get numero(): number {
+		return this._numero;
+	}
+
+	public get agencia(): number {
+		return this._agencia;
+	}
+
+	public get tipo(): number {
+		return this._tipo;
+	}
+
+	public get titular(): string {
+		return this._titular;
+	}
+
+	public get saldo(): number {
+		return this._saldo;
+	}
+
+	public set numero(numero: number) {
+		this._numero = numero;
+	}
+
+	public set agencia(agencia: number) {
+		this._agencia = agencia;
+	}
+
+	public set tipo(tipo: number) {
+		this._tipo = tipo;
+	}
+
+	public set titular(titular: string) {
+		this._titular = titular;
+	}
+
+	public set saldo(saldo: number) {
+		this._saldo = saldo;
+	}
+    
+// metodos auxiliares
+
+public sacar(valor:number): boolean {
+    if(valor <= 0){
+            console.log(Colors.fg.red, "O valor deve ser positivo", Colors.reset)
+            return false
+        }
+        if(valor > this._saldo){
+            console.log(Colors.fg.red, "Saldo Insuficiente!", Colors.reset)
+            return false
+        }
+        this._saldo -= valor
+        return true
+    }
+
+ public depositar(valor: number): void {
+    if(valor <= 0)
+        console.log(Colors.fg.red, "O valor deve ser positivo", Colors.reset)
+    else
+        this._saldo += valor
+}
+
+
+public visualizar(): void{
+    let tipo: string
+
+        switch(this._tipo){
+            case 1:
+                tipo = "Conta Corrente"
+            break
+            case 2:
+                tipo = "Conta Poupança"
+            break
+            default:
+                tipo = "Tipo Inválido"
+        }
+
+        console.log('\n************************************')
+        console.log('        DADOS DA CONTA              ')
+        console.log('************************************')
+        console.log(`Número da conta: ${this._numero}`)
+        console.log(`Número da agência: ${this._agencia}`)
+        console.log(`Nome do titular: ${this._titular}`)
+        console.log(`Tipo da conta: ${tipo}`)
+        console.log(`Saldo da conta: R$ ${this._saldo.toFixed(2)}`)
+}
 }
